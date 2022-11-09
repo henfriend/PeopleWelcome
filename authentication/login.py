25f4520ee09c6b244de5e258b15e0dbada5b8176
avnimungra <51969292+avnimungra@users.noreply.github.com>
2022-11-08 19:40:18 -0800

    login authentication

---
+++ authentication/login.py
@@ -0,0 +1,26 @@
+#test login authentication
+
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('estingConfig')
+        return app
+
+    def test_app_is_testing(self):
+        self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'auth_test'
+        )
+
+class TestDevelopmentConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('')
+        return app
+
+    def test_app_is_development(self):
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertFalse(current_app is None)
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'auth'
+        )
+
+
